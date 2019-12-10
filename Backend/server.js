c42fa8448613e721aaaf336a9a28217efea82399
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-10 16:10:51 +0900

    Create the Routers

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,20 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// call the routes {무조건 module exports 를 router로 해야한다}
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 // Connect the DataBase
 connectDB();
+
+// Routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
