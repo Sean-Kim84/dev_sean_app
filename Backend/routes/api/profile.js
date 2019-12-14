cd5302eb9291017a222fd91de65585b6276354e6
Sean-Kim84 <sean.kim843@gmail.com>
2019-12-14 13:12:23 +0900

    Delete Profile Experience

--- Backend/routes/api/profile.js
+++ Backend/routes/api/profile.js
@@ -177,11 +177,28 @@ router.put('/experience', [
       
       await profile.save();
       res.json(profile);
-    } catch(err){
+    } catch(err){ 
         console.error(err);
         res.status(500).send('Server Error');
     }
+});
 
-})
+//@route DELETE api/profile/experience
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get Remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+
+  } catch(err){
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
